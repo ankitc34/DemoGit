da133a9ff484abfc22774b531063530815bf10a2
ankitc34 <sunankiet@gmail.com>
2020-05-10 13:19:51 +0530

    code added by 2nd tester

--- src/test/java/qaclickacademy/SeleniumTest.java
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -6,7 +6,10 @@ public class SeleniumTest {
 	
 	@Test
 	public void browserAutomation() {
-		System.out.println("browserAutomation");
+		System.out.println("browserAutomation1");
+		System.out.println("browserAutomation2");
+		System.out.println("browserAutomation3");
+		System.out.println("browserAutomation4");
 		
 	}
 	
